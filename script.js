d10f49a1b802a35a7db14bee7f3383de443150f9
Memory-Wabwile <memorywabwile@gmail.com>
2021-12-12 17:03:21 +0300

    add alert message to the date

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
-let daysOfWeek = ["sunday" , "monday" , "tuesday" , "wednesday" , "thursday" , "friday" , "saturday"];
-let akanMaleNames = ["Kwasi" , "Kwadwo" , "Kwabena" , "Kwaku" , "Yaw" , "Kofi" , "Kwame"];
-let akanFemaleNames = ["Akosua" , "Adwoa" , "Abenaa" , "Akua" , "Yaa" , "Afua" , "Ama"];
+let daysOfWeek = [
+  "sunday",
+  "monday",
+  "tuesday",
+  "wednesday",
+  "thursday",
+  "friday",
+  "saturday",
+];
+let akanMaleNames = [
+  "Kwasi",
+  "Kwadwo",
+  "Kwabena",
+  "Kwaku",
+  "Yaw",
+  "Kofi",
+  "Kwame",
+];
+let akanFemaleNames = [
+  "Akosua",
+  "Adwoa",
+  "Abenaa",
+  "Akua",
+  "Yaa",
+  "Afua",
+  "Ama",
+];
 
 // Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
 //  CC - is the century digits. For example 1989 has CC = 19
 //  YY - is the Year digits (1989 has YY = 89)
 //  MM -  is the Month
-//  DD - is the Day of the month 
+//  DD - is the Day of the month
 //  mod - is the modulus function ( % )
 
 // let d, CC, YY, MM, DD;
 
-function validate(){
-    var date = document.getElementsByName("date");
-    if(document.forms.date.placeholder > 31 || document.forms.date.placeholder <= 0){
-        alert("please provide a valid date i.e from 0-31 !!");
-        document.forms.date.focus() ;
-        return false;
-    }
+function validate() {
+  var date = document.getElementsByName("date");
+  if (document.forms.date.value > 31 || document.forms.date.value <= 0) {
+    alert("please provide a valid date i.e from 0-31 !!");
+  }
 }
