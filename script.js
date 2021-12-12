5866d4235e6de5cddedbca713216d1dc1b30bc98
Memory-Wabwile <memorywabwile@gmail.com>
2021-12-12 10:08:18 +0300

    add array for days of wee, akan male names and akan female names

---
+++ script.js
@@ -0,0 +1,3 @@
+let daysOfWeek = ["sunday" , "monday" , "tuesday" , "wednesday" , "thursday" , "friday" , "saturday"];
+let akanMaleNames = ["Kwasi" , "Kwadwo" , "Kwabena" , "Kwaku" , "Yaw" , "Kofi" , "Kwame"];
+let akanFemaleNames = ["Akosua" , "Adwoa" , "Abenaa" , "Akua" , "Yaa" , "Afua" , "Ama"];
